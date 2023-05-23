55f09526f8eabc53457fed2efc227cd6b6e9e481
Guillermo Salinas <celacba@gmail.com>
2023-05-22 21:31:43 -0300

    add formulario contact

---
+++ static/js/correo.js
@@ -0,0 +1,13 @@
+const btnEnvio = document.getElementById('sendEmail');
+ 
+btnEnvio.addEventListener('click', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    window.location.href = `mailto:salguillermo69@gmail.com?subject=envioDesdeFormulario&body=Nombre%3A${name}%0ACorreo%3A${email}%0AMensaje%3A${message}`;
+
+    formReset.reset();
+     
+});
+
